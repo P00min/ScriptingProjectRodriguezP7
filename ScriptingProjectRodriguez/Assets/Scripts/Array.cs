1db7998ce7295d423abaf773c5abcd2ecf91fa50
P00min <718780@student.dusd.net>
2024-04-26 15:18:57 -0700

    Arrays

--- ScriptingProjectRodriguez/Assets/Scripts/Array.cs
+++ ScriptingProjectRodriguez/Assets/Scripts/Array.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Array : MonoBehaviour
 {
+    public GameObject[] players;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            Debug.Log("Player Number " + i + " is named " + players[i].name);
+        }
     }
 
     // Update is called once per frame
